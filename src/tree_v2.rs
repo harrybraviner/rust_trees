8ee11c0724ae01542783b5c1babf07b55f030b7b
Harry J. Braviner <harry.braviner@gmail.com>
2019-09-02 20:27:55 +0100

    Terse construction method

--- src/tree_v2.rs
+++ src/tree_v2.rs
@@ -9,6 +9,18 @@ impl<T> BinaryTree<T> {
     pub fn leaf(t: T) -> BinaryTree<T> {
         BinaryTree { left_link: None, right_link: None, payload: t }
     }
+
+    pub fn with_left_child(left_child: BinaryTree<T>, payload: T) -> BinaryTree<T> {
+        BinaryTree { left_link: Some(Box::new(left_child)), right_link: None, payload: payload }
+    }
+
+    pub fn with_right_child(right_child: BinaryTree<T>, payload: T) -> BinaryTree<T> {
+        BinaryTree { left_link: None, right_link: Some(Box::new(right_child)), payload: payload }
+    }
+
+    pub fn with_two_children(left_child: BinaryTree<T>, right_child: BinaryTree<T>, payload: T) -> BinaryTree<T> {
+        BinaryTree { left_link: Some(Box::new(left_child)), right_link: Some(Box::new(right_child)), payload: payload }
+    }
 }
 
 #[cfg(test)]
@@ -40,6 +52,28 @@ mod tests {
             })),
             payload: 0i32
         };
+    }
+
+    #[test]
+    fn construct_many_node_tree_terse() {
+        // Same as previous test, but hopefuly more terse.
+        //     0
+        //    / \
+        //   3   4
+        //  / \   \
+        // 1   2   5
+        BinaryTree::with_two_children(
+            BinaryTree::with_two_children(
+                BinaryTree::leaf(1i32),
+                BinaryTree::leaf(2i32),
+                3i32
+            ),
+            BinaryTree::with_right_child(
+                BinaryTree::leaf(5i32),
+                4i32
+            ),
+            0i32
+        );
     }
 }
 
