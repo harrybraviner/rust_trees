bc4ccf253dda70e5f06adef5add71dd20d496c4b
Harry J. Braviner <harry.braviner@gmail.com>
2019-09-02 18:56:04 +0100

    Added inorder traversal

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,21 @@
-struct BinaryTree<'a> {
+pub struct BinaryTree<'a> {
     left_link : Option<&'a BinaryTree<'a>>,
     right_link: Option<&'a BinaryTree<'a>>,
     payload: &'a i32,
+}
+
+impl BinaryTree<'_> {
+    pub fn inorder_travsersal(&self, action: &mut dyn FnMut(&i32)) {
+        match self.left_link {
+            Some(n) => n.inorder_travsersal(action),
+            _ => {},
+        }
+        action(self.payload);
+        match self.right_link {
+            Some(n) => n.inorder_travsersal(action),
+            _ => {},
+        }
+    }
 }
 
 #[cfg(test)]
@@ -10,15 +24,32 @@ mod tests {
 
     #[test]
     fn construct_single_node() {
-        let single_node = BinaryTree { left_link: None, right_link: None, payload: &0i32 };
+        let _single_node = BinaryTree { left_link: None, right_link: None, payload: &0i32 };
     }
 
     #[test]
     fn construct_non_empty_left() {
-        let single_node = BinaryTree {
+        let _two_nodes = BinaryTree {
             left_link: None,
             right_link: Some(&BinaryTree{left_link: None, right_link: None, payload: &1i32}),
             payload: &0i32
         };
+    }
+
+    #[test]
+    fn test_inorder_traversal() {
+        let mut traversal_record : Vec<i32> = vec![];
+
+        let mut store_traversal = |x: &i32| { traversal_record.push(x.clone()) };
+        
+        let two_nodes = BinaryTree {
+            left_link: None,
+            right_link: Some(&BinaryTree{left_link: None, right_link: None, payload: &1i32}),
+            payload: &0i32
+        };
+
+        two_nodes.inorder_travsersal(&mut store_traversal);
+
+        assert_eq!(traversal_record, vec![0i32, 1i32]);
     }
 }
